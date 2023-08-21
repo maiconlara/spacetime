e95d4ab6a96888d984355fecfa91e2afa91a8f40
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-21 12:29:21 -0300

    expo init

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
-    <h1>Hello World</h1>
-  )
+    <div className="h-screen bg-zinc-900 p-6">
+      <h1 className="text-zinc-50 text-4xl font-bold">Sua capsula do tempo</h1>
+    </div>
+  );
 }
