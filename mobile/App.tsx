73c544c26010fe3de81c3be6dcb790a93425fe1f
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-22 10:08:53 -0300

    Assets + metro config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { Text, ImageBackground } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import blugBr from "./src/assets/blur-bg.png";
+import Stripes from "./src/assets/stripes.svg";
 
 export default function App() {
-  return (
-    
-    <View className='flex-1 bg-zinc-950 items-center justify-center' >
-      <Text className='text-zinc-50 font-bold text-5xl' >Hello World</Text>
-             <StatusBar style="light" translucent />
+  const [isLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
 
-    </View>
+  if (!isLoadedFonts) {
+    return null;
+  }
+  return (
+    <ImageBackground
+      alt="blur"
+      source={blugBr}
+      className="flex-1 bg-gray-900 items-center relative "
+      imageStyle={{
+        position: "absolute",
+        left: "-100%",
+      }}
+    >
+      <Stripes />
+      <StatusBar style="light" translucent />
+    </ImageBackground>
   );
 }
-
-
