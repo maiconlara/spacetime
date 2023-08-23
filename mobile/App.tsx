77f4e873189ff7c49c25f748184afdc389aca929
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-23 16:39:34 -0300

    gettin token in mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { View, ImageBackground, Text, TouchableOpacity } from "react-native";
+import * as SecureStore from "expo-secure-store";
+
 import {
   useFonts,
   Roboto_400Regular,
@@ -10,8 +12,17 @@ import blugBr from "./src/assets/blur-bg.png";
 import Stripes from "./src/assets/stripes.svg";
 import NLWLogo from "./src/assets/logo.svg";
 import { styled } from "nativewind";
+import { useAuthRequest, makeRedirectUri } from "expo-auth-session";
+import { useEffect, useState } from "react";
+import { api } from "./src/lib/api";
 
 const StyledStrypes = styled(Stripes);
+const discovery = {
+  authorizationEndpoint: "https://github.com/login/oauth/authorize",
+  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  revocationEndpoint:
+    "https://github.com/settings/connections/applications/b7ff17410e9d0877d96a",
+};
 
 export default function App() {
   const [isLoadedFonts] = useFonts({
@@ -20,9 +31,36 @@ export default function App() {
     BaiJamjuree_700Bold,
   });
 
+  const [request, response, signIn] = useAuthRequest(
+    {
+      clientId: "b7ff17410e9d0877d96a",
+      scopes: ["identity"],
+      redirectUri: makeRedirectUri({
+        scheme: "nwlspacetime",
+      }),
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      const { code } = response.params;
+      console.log("code: ", code);
+
+      api.post("/register", {
+          code,
+        })
+        .then((response) => {
+          const { token } = response.data;
+          SecureStore.setItemAsync("token", token);
+        });
+    }
+  }, [response]);
+
   if (!isLoadedFonts) {
     return null;
   }
+
   return (
     <ImageBackground
       alt="blur"
@@ -38,18 +76,27 @@ export default function App() {
       <View className="flex-1 items-center justify-center gap-6">
         <NLWLogo />
         <View className="space-y-2">
-          <Text className="text-center font-title text-2xl leading-tight text-gray-50">Sua cápsula do tempo</Text>
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
           <Text className="text-center font-body text-sm leading-relaxed text-gray-100">
             Colecione momentos marcantes da sua jornada e compartilhe (se
             quiser) com mundo!
           </Text>
         </View>
-        <TouchableOpacity activeOpacity={0.6} className="rounded-full bg-green-500 px-5 py-2">
-              <Text className="font-alt text-sm uppercase text-black">Cadastrar lembrança</Text>
+        <TouchableOpacity
+          activeOpacity={0.6}
+          className="rounded-full bg-green-500 px-5 py-2"
+          onPress={() => signIn()}
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar lembrança
+          </Text>
         </TouchableOpacity>
-
       </View>
-      <Text className="text-center font-body leading-relaxed text-gray-200">Feito com 💜 por Maicon Lara</Text>
+      <Text className="text-center font-body leading-relaxed text-gray-200">
+        Feito com 💜 por Maicon Lara
+      </Text>
 
       <StatusBar style="light" translucent />
     </ImageBackground>
