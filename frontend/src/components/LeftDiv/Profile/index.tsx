b45e1e9737e020b57fd81f42edd06055bdd948df
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-23 14:42:40 -0300

    profile component

--- frontend/src/components/LeftDiv/Profile/index.tsx
+++ frontend/src/components/LeftDiv/Profile/index.tsx
@@ -8,7 +8,8 @@ const Profile = () => {
     <div className="flex  items-center gap-3 text-left">
       <Image src={avatarUrl} alt="" width={80} height={80} className="w-10 h-10 rounded-full" />
       <p className="text-sm leading-snug max-w-[140px]">
-        {name}
+        Bem-vindo {name}!
+        <a className="block text-red-400 hover:text-red-300 cursor-pointer pt-1">Sair</a>
       </p>
     </div>
   );
