688e76d6bfc26f24b1e4b7fd816067b0f93cbf9a
maiconlara <maiconluan.lara@yahoo.com.br>
2023-08-23 12:54:41 -0300

    creating frontend api

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,23 +1,28 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Bai_Jamjuree as Bai, Roboto_Flex as Roboto } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Bai_Jamjuree as Bai, Roboto_Flex as Roboto } from "next/font/google";
 
-const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' });
-const bai = Bai({ subsets: ['latin'], weight: '700', variable:'--font-bai' });
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
+const bai = Bai({ subsets: ["latin"], weight: "700", variable: "--font-bai" });
 
 export const metadata: Metadata = {
-  title: 'Spacetime',
-  description: 'Time capsule made with React, Next.js, Tailwind CSS and TypeScript', 
-}
+  title: "Spacetime",
+  description:
+    "Time capsule made with React, Next.js, Tailwind CSS and TypeScript",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${bai.variable} font-sans text-gray-100 bg-gray-900`}>{children}</body>
+      <body
+        className={`${roboto.variable} ${bai.variable} font-sans text-gray-100 bg-gray-900`}
+      >
+        {children}
+      </body>
     </html>
-  )
+  );
 }
